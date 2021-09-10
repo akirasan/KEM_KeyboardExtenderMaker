6daf1cb1917e022ffc2a27aedfde04b48cc99d16
akirasan <akirasan21h@gmail.com>
2021-09-10 21:19:16 +0200

    separated config keys

--- src/main.cpp
+++ src/main.cpp
@@ -18,35 +18,7 @@
 
 KeyboardExtMaker KEM;
 
-void setup_Keys()
-{
-
-  KEM.setKeyFunc(KEM_KEY1, ADD_CUBE_BLENDER, "add cube");
-  KEM.setColorPress(KEM_KEY1, 255, 255, 255);
-
-  KEM.setKey(KEM_KEY2, PIN_KEY2, LED_KEY2, MODE_HOLDED);
-  KEM.setColorComand1(KEM_KEY2, 255, 0, 0);
-  KEM.setKeyFuncComand1(KEM_KEY2, Send_KEY2_func1, "key 2 f1");
-
-  KEM.setColorComand2(KEM_KEY2, 0, 255, 0);
-  KEM.setKeyFuncComand2(KEM_KEY2, Send_KEY2_func2, "key 2 f2");
-
-  KEM.setKeyFunc(KEM_KEY3, HelloWorld, "Hello World");
-  KEM.setColorPress(KEM_KEY3, 255, 255, 255);
-
-  KEM.setKeyFunc(KEM_KEY4, ALT_TAB, "ALT + TAB");
-  KEM.setColorPress(KEM_KEY4, 255, 5, 5);
-
-  KEM.setColorPress(KEM_KEY5, 255, 255, 255);
-  KEM.setColorPress(KEM_KEY6, 255, 255, 255);
-  KEM.setColorPress(KEM_KEY7, 255, 255, 255);
-  KEM.setColorPress(KEM_KEY8, 255, 255, 255);
-
-  KEM.setColorDefault(0, 0, 50);
-  KEM.setColorBase(50, 0, 255);
-  
-  allColorBASE(50, 0, 150);
-}
+#include "keys_config.h"
 
 void setup()
 {
